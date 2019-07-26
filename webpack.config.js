0b98c582a54718ac37c59d8413a31dd8f53461d5
bernethe <bernethe@bernethe.local>
2019-07-26 16:06:13 -0600

    WebPack listo

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,43 @@
 const path = require('path');
+const HtmlWebPackplugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './scr/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackplugin(
+            {
+                template: "./public/index.html",
+                file: "./index.html"
+            }
+        )
+    ]
+}
